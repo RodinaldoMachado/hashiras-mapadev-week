0e89792da265f303b5e628931c71dd32439e1210
RodinaldoMachado <110370919+RodinaldoMachado@users.noreply.github.com>
2022-08-01 02:17:19 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,35 @@
+const btnAvancar = document.getElementById('btn-avancar');
+const btnVoutar = document.getElementById('btn-voutar');
+const cartoes = document.querySelectorAll('.cartao');
+
+let cartaoAtual = 0;
+
+
+function esconderCartaoSelecionado() {
+
+    const cartaoSelecionado = document.querySelector('.selecionado');
+    cartaoSelecionado.classList.remove('selecionado');
+}
+
+function mostrarCartao(indiceCartao) {
+    cartoes[indiceCartao].classList.add('selecionado');
+}
+
+btnAvancar.addEventListener('click', function () {
+    if (cartaoAtual === cartoes.length - 1) return;
+
+    esconderCartaoSelecionado();
+    cartaoAtual++;
+    mostrarCartao(cartaoAtual);
+});
+
+btnVoutar.addEventListener('click', function () {
+    if (cartaoAtual === 0) return;
+
+    esconderCartaoSelecionado()
+    cartaoAtual--;
+    mostrarCartao(cartaoAtual);
+
+
+
+});
